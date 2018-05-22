96e1921a31a3b7881924242468b127ed02ef3b40
Francisco Martin Aguilar <fcomartinaguilar@gmail.com>
2018-05-22 21:14:58 +0200

    validaciones 1

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
+})
+export class TemplateComponent implements OnInit {
+
+  usuario:Object = {
+    nombre: "Curro",
+    apellido: "Martin",
+    email: "fcocurrelas@gmail.com"
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma:NgForm){
+    console.log("ngForm", forma);
+    console.log("Valor de la forma", forma.value);
+
+    console.log("Usuario", this,this.usuario);
+    
+    
+  }
+
+}
